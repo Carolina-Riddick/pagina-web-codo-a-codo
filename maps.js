39748e518cdb03887464ca818cb94a2441be0895
Carolina Riddick <carolinariddick@gmail.com>
2022-10-26 12:04:09 -0300

    API maps

---
+++ maps.js
@@ -0,0 +1,32 @@
+function iniciarMaps(){
+    var coord = {lat:-34.597559 , lng:-58.3909937};
+    var map = new google.maps.Map(document.getElementById("maps"),{
+        zoom: 15,
+        center: coord
+    });
+    var marker = new google.maps.Marker({
+        position : coord,
+        map : map
+    });
+}
+
+
+
+
+
+
+/*
+function iniciarMaps(){
+    var coord = {lat: -34.597559, lng: -58.3909937};
+    var map = new google.maps.Map(document.getElementById("maps"),{
+        zoom: 15,
+        center: coord
+    });
+    var marker = new google.maps.Marker({
+        position: coord,
+        map : map
+    });
+}
+
+
+*/
