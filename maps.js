8f1461cfddae2b63a73cd86132395452d2b707ec
Geraldine <geraldine.nasa@gmail.com>
2022-10-26 16:41:12 -0300

    cambios float

--- maps.js
+++ maps.js
@@ -1,6 +1,6 @@
 /*Funcion Maps*/
  
-function iniciarMaps(){
+function iniciarMaps() {
     var coord = {lat:-34.597559 , lng:-58.3909937};
     var map = new google.maps.Map(document.getElementById("maps"),{
         zoom: 15,
